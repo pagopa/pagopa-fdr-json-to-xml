4b6c0e3fc9ba63e1a4d3d07daccf0a4841216656
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-04-09 15:19:33 +0200

    [PAGOPA-2745] feat: Add version to HTTP Info

--- src/main/java/it/gov/pagopa/fdr/conversion/Info.java
+++ src/main/java/it/gov/pagopa/fdr/conversion/Info.java
@@ -4,8 +4,12 @@ import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import it.gov.pagopa.fdr.conversion.model.AppInfo;
 
+import java.io.InputStream;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 
 /**
@@ -25,8 +29,34 @@ public class Info {
 			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 
-		return request.createResponseBuilder(HttpStatus.OK).build();
+		return request
+				.createResponseBuilder(HttpStatus.OK)
+				.header("Content-Type", "application/json")
+				.body(
+						getInfo(
+								context.getLogger(),
+								"/META-INF/maven/it.gov.pagopa.fdr.conversion/pom.properties"))
+				.build();
 	}
 
+	public synchronized AppInfo getInfo(Logger logger, String path) {
+		String version = null;
+		String name = null;
+		try {
+			Properties properties = new Properties();
+			InputStream inputStream = loadResource(path);
+			if (inputStream != null) {
+				properties.load(inputStream);
+				version = properties.getProperty("version", null);
+				name = properties.getProperty("artifactId", null);
+			}
+		} catch (Exception e) {
+			logger.severe("Impossible to retrieve information from pom.properties file.");
+		}
+		return AppInfo.builder().version(version).environment("azure-fn").name(name).build();
+	}
 
+	public InputStream loadResource(String path) {
+		return getClass().getResourceAsStream(path);
+	}
 }
