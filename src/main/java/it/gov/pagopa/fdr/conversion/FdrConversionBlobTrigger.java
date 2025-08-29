38e9c21c4da4a3b4b3d0265a11c12caa846bb9b9
giomella <giomella@emeal.nttdata.com>
2025-08-29 16:56:02 +0200

    [PAGOPA-2800] format code

--- src/main/java/it/gov/pagopa/fdr/conversion/FdrConversionBlobTrigger.java
+++ src/main/java/it/gov/pagopa/fdr/conversion/FdrConversionBlobTrigger.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.fdr.conversion;
 
+import static it.gov.pagopa.fdr.conversion.exception.AlertAppException.getExceptionDetails;
+
 import com.google.gson.JsonObject;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.*;
@@ -7,94 +9,125 @@ import feign.Feign;
 import feign.FeignException;
 import it.gov.pagopa.fdr.conversion.client.FdR1Client;
 import it.gov.pagopa.fdr.conversion.exception.AlertAppException;
-import it.gov.pagopa.fdr.conversion.util.StorageAccountUtil;
 import it.gov.pagopa.fdr.conversion.model.ErrorEnum;
-import lombok.extern.slf4j.Slf4j;
-
+import it.gov.pagopa.fdr.conversion.util.StorageAccountUtil;
 import java.util.Base64;
 import java.util.Map;
-
-import static it.gov.pagopa.fdr.conversion.exception.AlertAppException.getExceptionDetails;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FdrConversionBlobTrigger {
-    private static final String FN_NAME = "FdR3-to-FdR1";
-    private static final Integer MAX_RETRY_COUNT = 4;
-    private static final String ELABORATE_KEY = "elaborate";
-    private static final String SESSION_ID_METADATA_KEY = "sessionId";
+  private static final String FN_NAME = "FdR3-to-FdR1";
+  private static final Integer MAX_RETRY_COUNT = 4;
+  private static final String ELABORATE_KEY = "elaborate";
+  private static final String SESSION_ID_METADATA_KEY = "sessionId";
 
-    public final String fdrFase1BaseUrl = System.getenv("FDR_FASE1_BASE_URL");
-    private final String fdrFase1ApiKey = System.getenv("FDR_FASE1_API_KEY");
+  public final String fdrFase1BaseUrl = System.getenv("FDR_FASE1_BASE_URL");
+  private final String fdrFase1ApiKey = System.getenv("FDR_FASE1_API_KEY");
 
-    /**
-     * The job of this function is to convert, i.e., store and save, the streams of FdR3 to FdR1.
-     * This is done by calling the API provided by FdR1
-     * @param content
-     * @param blobName
-     * @param blobMetadata
-     * @param context
-     *
-     * Retry mechanism focus
-     * ExponentialBackoffRetry will perform N = 5 backoff retries multiplied by N = 1 retries
-     * via poisonBlobThreshold mechanism specified in the host.json,
-     * in total 5 retries and 1 write attempts on dead-letter that will overwrite the same record in the table.
-     * Specifically these will be the retry index values = 0, 1, 2, 3, 4
-     */
-    @FunctionName("BlobEventProcessor")
-    @ExponentialBackoffRetry(maxRetryCount = 4, maximumInterval = "00:05:00", minimumInterval = "00:00:10")
-    public boolean process (
-            @BlobTrigger(
-                    name = "Fdr3BlobTrigger",
-                    dataType = "binary",
-                    path = "%BLOB_STORAGE_FDR3_CONTAINER%/{blobName}",
-                    connection = "FDR_SA_CONNECTION_STRING")
-            byte[] content,
-            @BindingName("blobName") String blobName,
-            @BindingName("Metadata") Map<String, String> blobMetadata,
-            final ExecutionContext context) {
-        int retryIndex = context.getRetryContext() == null ? -1 : context.getRetryContext().getRetrycount();
-        String iid = context.getInvocationId();
-        log.info("[{}] Triggered, id = {}, blob-name = {}, blob-metadata = {}, retry = {}", FN_NAME, iid, blobName, blobMetadata, retryIndex);
+  /**
+   * The job of this function is to convert, i.e., store and save, the streams of FdR3 to FdR1. This
+   * is done by calling the API provided by FdR1
+   *
+   * @param content FDR3 flow from blob storage
+   * @param blobName FDR3 flow blob name
+   * @param blobMetadata FDR3 flow blob metadata
+   * @param context
+   *     <p>Retry mechanism focus ExponentialBackoffRetry will perform N = 5 backoff retries
+   *     multiplied by N = 1 retries via poisonBlobThreshold mechanism specified in the host.json,
+   *     in total 5 retries and 1 write attempts on dead-letter that will overwrite the same record
+   *     in the table. Specifically these will be the retry index values = 0, 1, 2, 3, 4
+   */
+  @FunctionName("BlobEventProcessor")
+  @ExponentialBackoffRetry(
+      maxRetryCount = 4,
+      maximumInterval = "00:05:00",
+      minimumInterval = "00:00:10")
+  public boolean process(
+      @BlobTrigger(
+              name = "Fdr3BlobTrigger",
+              dataType = "binary",
+              path = "%BLOB_STORAGE_FDR3_CONTAINER%/{blobName}",
+              connection = "FDR_SA_CONNECTION_STRING")
+          byte[] content,
+      @BindingName("blobName") String blobName,
+      @BindingName("Metadata") Map<String, String> blobMetadata,
+      final ExecutionContext context) {
+    int retryIndex =
+        context.getRetryContext() == null ? -1 : context.getRetryContext().getRetrycount();
+    String iid = context.getInvocationId();
+    log.info(
+        "[{}] Triggered, id = {}, blob-name = {}, blob-metadata = {}, retry = {}",
+        FN_NAME,
+        iid,
+        blobName,
+        blobMetadata,
+        retryIndex);
 
-        // Ignore the blob if it does not contain the elaborate key or if it isn't true
-        if (!Boolean.parseBoolean(blobMetadata.getOrDefault(ELABORATE_KEY, "false"))) {
-            log.info("[{}] Skipped, id = {}, blob-name = {}, blob-metadata = {}, retry = {}", FN_NAME, iid, blobName, blobMetadata, retryIndex);
-            return false;
-        }
-
-        // Retry is configured at function level, we always make the exception throw to trigger that retry
-        try {
-            FdR1Client fdR1Client = Feign.builder().target(FdR1Client.class, fdrFase1BaseUrl);
-            fdR1Client.postConversion(fdrFase1ApiKey, getPayload(content));
-            log.info("[{}][id={}] Successful conversion call to FdR1", FN_NAME, context.getInvocationId());
-        }
-        catch (Exception e) {
-            log.error("[Exception][{}][id={}] class = {}, message = {}", FN_NAME, iid, e.getClass(), e.getMessage());
-            // Get error type
-            ErrorEnum errorType = (e instanceof FeignException) ? ErrorEnum.HTTP_ERROR : ErrorEnum.GENERIC_ERROR;
-            // Last retry check
-            if (retryIndex >= MAX_RETRY_COUNT) {
-                sendToDeadLetter(context, blobName, blobMetadata, e.getMessage(), errorType, e.getMessage(), e);
-                String exceptionDetails = getExceptionDetails(blobName, blobMetadata.get(SESSION_ID_METADATA_KEY), retryIndex);
-                throw new AlertAppException(e.getMessage(), e.getCause(), exceptionDetails);
-            }
-            throw e;
-        }
-        return true;
+    // Ignore the blob if it does not contain the elaborate key or if it isn't true
+    if (!Boolean.parseBoolean(blobMetadata.getOrDefault(ELABORATE_KEY, "false"))) {
+      log.info(
+          "[{}] Skipped, id = {}, blob-name = {}, blob-metadata = {}, retry = {}",
+          FN_NAME,
+          iid,
+          blobName,
+          blobMetadata,
+          retryIndex);
+      return false;
     }
 
-    // Create JSON object with the base64 encoded payload
-    public static String getPayload(byte[] content) {
-        String base64Content = Base64.getEncoder().encodeToString(content);
-        JsonObject jsonBody = new JsonObject();
-        jsonBody.addProperty("payload", base64Content);
-        jsonBody.addProperty("encoding", "base64");
-        return jsonBody.toString();
+    // Retry is configured at function level, we always make the exception throw to trigger that
+    // retry
+    try {
+      FdR1Client fdR1Client = Feign.builder().target(FdR1Client.class, fdrFase1BaseUrl);
+      fdR1Client.postConversion(fdrFase1ApiKey, getPayload(content));
+      log.info(
+          "[{}][id={}] Successful conversion call to FdR1", FN_NAME, context.getInvocationId());
+    } catch (Exception e) {
+      log.error(
+          "[Exception][{}][id={}] class = {}, message = {}",
+          FN_NAME,
+          iid,
+          e.getClass(),
+          e.getMessage());
+      // Get error type
+      ErrorEnum errorType =
+          (e instanceof FeignException) ? ErrorEnum.HTTP_ERROR : ErrorEnum.GENERIC_ERROR;
+      // Last retry check
+      if (retryIndex >= MAX_RETRY_COUNT) {
+        sendToDeadLetter(
+            context, blobName, blobMetadata, e.getMessage(), errorType, e.getMessage(), e);
+        String exceptionDetails =
+            getExceptionDetails(blobName, blobMetadata.get(SESSION_ID_METADATA_KEY), retryIndex);
+        throw new AlertAppException(e.getMessage(), e.getCause(), exceptionDetails);
+      }
+      throw e;
     }
+    return true;
+  }
 
-    // Check retry index and save to dead-letter if max-retry has been reached
-    private static void sendToDeadLetter(ExecutionContext context, String blob, Map<String, String> metadata, String message, ErrorEnum errorEnum, String response, Exception e) {
-        log.warn("[ALERT][{}][LAST_RETRY][DEAD-LETTER] Performed last retry for event ingestion: InvocationId [{}]", FN_NAME, context.getInvocationId());
-        StorageAccountUtil.sendToErrorTable(context, blob, metadata, message, errorEnum, response, e);
-    }
+  // Create JSON object with the base64 encoded payload
+  public static String getPayload(byte[] content) {
+    String base64Content = Base64.getEncoder().encodeToString(content);
+    JsonObject jsonBody = new JsonObject();
+    jsonBody.addProperty("payload", base64Content);
+    jsonBody.addProperty("encoding", "base64");
+    return jsonBody.toString();
+  }
+
+  // Check retry index and save to dead-letter if max-retry has been reached
+  private static void sendToDeadLetter(
+      ExecutionContext context,
+      String blob,
+      Map<String, String> metadata,
+      String message,
+      ErrorEnum errorEnum,
+      String response,
+      Exception e) {
+    log.warn(
+        "[ALERT][{}][LAST_RETRY][DEAD-LETTER] Performed last retry for event ingestion: InvocationId [{}]",
+        FN_NAME,
+        context.getInvocationId());
+    StorageAccountUtil.sendToErrorTable(context, blob, metadata, message, errorEnum, response, e);
+  }
 }
--- src/main/java/it/gov/pagopa/fdr/conversion/FdrRetryAllHttpTrigger.java
+++ src/main/java/it/gov/pagopa/fdr/conversion/FdrRetryAllHttpTrigger.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.fdr.conversion;
 
+import static it.gov.pagopa.fdr.conversion.util.StorageAccountUtil.removeEntity;
+
 import com.azure.core.http.rest.PagedIterable;
 import com.azure.core.http.rest.PagedResponse;
 import com.azure.data.tables.models.TableEntity;
@@ -10,56 +12,73 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 import it.gov.pagopa.fdr.conversion.model.BlobData;
 import it.gov.pagopa.fdr.conversion.model.ErrorTableColumns;
 import it.gov.pagopa.fdr.conversion.util.StorageAccountUtil;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Map;
 import java.util.Optional;
-import java.util.logging.Logger;
-
-import static it.gov.pagopa.fdr.conversion.util.StorageAccountUtil.removeEntity;
-
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FdrRetryAllHttpTrigger {
-    private static final String FN_NAME = "ErrorRetryAllFunction";
+  private static final String FN_NAME = "ErrorRetryAllFunction";
 
-    @FunctionName("ErrorRetryAllFunction")
-    public HttpResponseMessage process (
-            @HttpTrigger(name = "ErrorRetryAllFunctionHttpTrigger",
-                    methods = {HttpMethod.GET},
-                    route = "errors/retry",
-                    authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        try {
-            PagedIterable<TableEntity> entities = StorageAccountUtil.getTableEntities();
-            assert entities != null;
+  @FunctionName("ErrorRetryAllFunction")
+  public HttpResponseMessage process(
+      @HttpTrigger(
+              name = "ErrorRetryAllFunctionHttpTrigger",
+              methods = {HttpMethod.GET},
+              route = "errors/retry",
+              authLevel = AuthorizationLevel.ANONYMOUS)
+          HttpRequestMessage<Optional<String>> request,
+      final ExecutionContext context) {
+    try {
+      PagedIterable<TableEntity> entities = StorageAccountUtil.getTableEntities();
+      assert entities != null;
 
-            // Iterate through the pages of results
-            for (PagedResponse<TableEntity> page : entities.iterableByPage()) {
-                // For each page, process the retry
-                for (TableEntity e : page.getElements()) {
-                    Map<String, Object> properties = e.getProperties();
-                    String blobName = (String) properties.get(ErrorTableColumns.COLUMN_FIELD_BLOB);
-                    BlobData blobData = StorageAccountUtil.getBlobContent(blobName);
-                    FdrConversionBlobTrigger processor = new FdrConversionBlobTrigger();
-                    assert blobData != null;
-                    boolean processed = processor.process(blobData.getContent(), blobName, blobData.getMetadata(), context);
-                    if(processed) removeEntity(context, blobData.getMetadata());
+      // Iterate through the pages of results
+      for (PagedResponse<TableEntity> page : entities.iterableByPage()) {
+        // For each page, process the retry
+        for (TableEntity e : page.getElements()) {
+          Map<String, Object> properties = e.getProperties();
+          String blobName = (String) properties.get(ErrorTableColumns.COLUMN_FIELD_BLOB);
+          BlobData blobData = StorageAccountUtil.getBlobContent(blobName);
+          FdrConversionBlobTrigger processor = new FdrConversionBlobTrigger();
+          assert blobData != null;
+          boolean processed =
+              processor.process(blobData.getContent(), blobName, blobData.getMetadata(), context);
+          if (processed) removeEntity(context, blobData.getMetadata());
 
-                    log.info("[fn={}][id={}] Retry table entity processed = {}, PartitionKey = {}, RowKey = {}, Properties = {}, blobName = {}",
-                            FN_NAME, context.getInvocationId(), processed, e.getPartitionKey(), e.getRowKey(), properties, blobName);
-                }
-            }
-
-            return request.createResponseBuilder(HttpStatus.OK).body(HttpStatus.OK.toString()).build();
-        } catch (Exception e) {
-            log.error("[Exception][id={}] Error while FDR3 ErrorRetryFunction execution, class = {}, message = {}",
-                    context.getInvocationId(), e.getClass(), e.getMessage());
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body(HttpStatus.INTERNAL_SERVER_ERROR.toString()).build();
-        } catch (AssertionError e) {
-            log.error("[AssertionError][id={}] Error while FDR3 ErrorRetryFunction execution, class = {}, message = {}",
-                    context.getInvocationId(), e.getClass(), e.getMessage());
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body(HttpStatus.INTERNAL_SERVER_ERROR.toString()).build();
+          log.info(
+              "[fn={}][id={}] Retry table entity processed = {}, PartitionKey = {}, RowKey = {}, Properties = {}, blobName = {}",
+              FN_NAME,
+              context.getInvocationId(),
+              processed,
+              e.getPartitionKey(),
+              e.getRowKey(),
+              properties,
+              blobName);
         }
+      }
+
+      return request.createResponseBuilder(HttpStatus.OK).body(HttpStatus.OK.toString()).build();
+    } catch (Exception e) {
+      log.error(
+          "[Exception][id={}] Error while FDR3 ErrorRetryFunction execution, class = {}, message = {}",
+          context.getInvocationId(),
+          e.getClass(),
+          e.getMessage());
+      return request
+          .createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+          .build();
+    } catch (AssertionError e) {
+      log.error(
+          "[AssertionError][id={}] Error while FDR3 ErrorRetryFunction execution, class = {}, message = {}",
+          context.getInvocationId(),
+          e.getClass(),
+          e.getMessage());
+      return request
+          .createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+          .body(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+          .build();
     }
+  }
 }
